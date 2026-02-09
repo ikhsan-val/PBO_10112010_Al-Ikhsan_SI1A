f02fd5cb869981719cb566dc840619ec6ec56a67
ikhsan-val <ikhsanal719@gmail.com>
2026-02-09 12:44:38 +0700

    Add files via upload

--- pertemuan 1.1.php
+++ pertemuan 1.1.php
@@ -1,3 +1,12 @@
 <?php
-echo "ayo pkm yeyyyy";
+class belanja{
+    var $jumlah = 4;
+    var $hargasatuan = 1000;
+    var $namaBarang ="penghapus";
+      public function totalHarga(): float
+{
+    $subTotal = $this->jumlah * $this->hargasatuan;
+    return $subTotal;
+}
+}
 ?>
